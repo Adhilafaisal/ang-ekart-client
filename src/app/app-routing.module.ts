dac91dce9411a161359d85fcc7701ad57662400a
adhila <adhilafaisal7@gmail.com>
2024-06-10 16:17:03 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegComponent } from './reg/reg.component';
+import { DetailComponent } from './detail/detail.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'log',component:LoginComponent},
+  {path:'reg',component:RegComponent},
+  {path:'detail/:id',component:DetailComponent},
+  {path:'cart',component:CartComponent},
+  {path:'check',component:CheckoutComponent},
+  {path:'wish',component:WishlistComponent},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
